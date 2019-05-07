c7b5e400fc0f204bbcf40a996f7945bb0625dc34
YangLiJin <aiaiaiaiaiai0@qq.com>
2019-05-07 16:07:13 +0800

    new

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,23 +1,42 @@
 <!--  -->
 <template>
-    <div class="login">
-        <el-row>
-            <el-col :span="8"></el-col>
-            <el-col :span="8">
-                <el-form 
-                :model="form" 
-                :rules="formRules"
-                class="mmsFrom"
-                label-width="120px" >
-                <mms-form :items="items" :form-data="form">
-                </mms-form>
-                </el-form>
-                <div class="btn-box">
-                    <el-button type="primary" @click="login">登录</el-button>
+    <div class="login-page">
+        <div class="main">
+            <div class="logo-wrap">
+                <img class="logo" src="./image/logo.png" width="60" height="60">
+                <p class="text">后台管理系统</p>
+            </div>
+            <div class="banner">
+                <div class="right-bg"></div>
+                <div class="content">
+                    <div class="bg-img"></div>
+                    <div class="login-form">
+                        <el-tabs v-model="tabName">
+                            <el-tab-pane label="用户登陆" name="User" class="panel">
+                                <el-form 
+                                    :model="form" 
+                                    :rules="formRules"
+                                    class="mmsFrom normal"
+                                    label-width="60px" >
+                                    <mms-form :items="items" :form-data="form">
+                                    </mms-form>
+                                    <div class="btn-wrap">
+                                        <el-button
+                                            class="btn"
+                                            size="medium"
+                                            type="primary"
+                                            @click="login">
+                                            登 录
+                                        </el-button>
+                                    </div>
+                                </el-form>
+                            </el-tab-pane>
+                        </el-tabs>
+                    </div>
                 </div>
-            </el-col>
-            <el-col :span="8"></el-col>
-        </el-row>
+            </div>
+        </div>
+        <copyright></copyright>
     </div>
 </template>
 
@@ -28,6 +47,7 @@ export default {
     data() {
         return {
             form: initUser(),
+            tabName: 'User',
             items: [
                 {
                     label: '账号:',
@@ -68,4 +88,85 @@ export default {
 
 </script>
 <style lang='scss' scoped>
+@import 'sass/variables.scss';
+.login-page{
+    min-width: 1200px;
+    .main{
+        padding-bottom: $con-spacing-col-base;
+        .logo-wrap{
+            height: 60px;
+            line-height: 60px;
+            width: 1200px;
+            margin: 0 auto $con-spacing-col-base auto;
+            padding-top: $con-spacing-col-base;
+            .logo{
+                float: left;
+                margin-right: $con-spacing-col-lg;}
+            .text{
+                font-size: 24px;}
+        }
+        .banner{
+            position: relative;
+            height: 560px;
+            background-color: $color-theme-primary;
+            .right-bg,
+            .left-bg{
+                position: absolute;
+                width: 430px;
+                height: 300px;}
+            .left-bg{
+                top: 0;
+                left: 10;
+                background-image: url("./image/login-bg-left.png");}
+            .right-bg{
+                right: 0;
+                bottom: 0;
+                background-image: url("./image/login-bg-right.png");
+                background-position-x: 50px;
+                background-repeat: no-repeat;}
+            .content{
+                width: 1200px;
+                height: 560px;
+                margin: 0 auto;
+                position: relative;
+                overflow: hidden;
+                .bg-img{
+                    margin-top: 80px;
+                    margin-left: $con-spacing-col-lg;
+                    height: 400px;
+                    width: 700px;
+                    background-image: url("./image/login-bg.png");
+                    background-repeat: no-repeat;}
+                .login-form{
+                    position: absolute;
+                    width: 350px;
+                    top: 50%;
+                    right: 0px;
+                    transform: translateY(-50%);
+                    height: 320px;
+                    background: #fff;
+                    padding: $con-spacing-col-lg $con-spacing-col-base 0 $con-spacing-col-base;
+                    border-radius: 5px;}
+                    /deep/{
+                        .el-form-item__content,
+                        .el-form-item__label{
+                            font-size: $con-font-size-nm !important;
+                        }
+                        .el-input__inner{
+                            height: 30px;
+                            line-height: 30px;
+                        }
+                    } 
+                    .panel{
+                        padding: $con-spacing-col-base 20px;
+                            margin-bottom: 30px;}
+                        .btn-wrap{
+                            text-align: center;
+                            margin-top: $con-spacing-col-base;}
+                            .btn{
+                                width: 100%;}
+            }
+        }
+    }
+}
 </style>
